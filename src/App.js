86e88eba0e9059b430eddcba2b1c78e422701b61
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-12-18 21:45:49 +0530

    React Create Element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Hello, Hellonew } from "./components/hello.jsx";
 import Trial from "./components/class.jsx";
+import { Trialnew, TrialFunc } from "./components/createreact.jsx";
 function App() {
   return (
     <div className="App">
       <Hello />
       <Hellonew />
       <Trial />
+      <Trialnew />
+      <TrialFunc />
     </div>
   );
 }
